a91f6ea4185f03dc3c6b449ff565074690dc582c
Azure Dust <azurexsx@gmail.com>
2023-04-11 05:57:28 +0200

    Add project files.

---
+++ Random C/MainWindow.xaml.cs
@@ -0,0 +1,96 @@
+﻿using SimpleTCP;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Random_C
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        SimpleTcpClient client = new SimpleTcpClient();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            client.DataReceived += (sender, e) =>
+            {
+                var msg = Encoding.UTF8.GetString(e.Data);
+
+                if (msg.Contains("-1"))
+                {
+                    try
+                    {
+                        MessageBox.Show("Disconnected");
+                        client.Disconnect();
+                    }
+                    catch { }
+                }
+                else
+                {
+                    this.Dispatcher.Invoke(new Action(() =>
+                    {
+                        TXT.Text += msg + "\n\n";
+                    }));
+                  
+                }
+               
+            };
+            try
+            {
+                client.Connect("127.0.0.1", 5000);
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                this.Close();
+            }
+          
+        }
+
+        private void Button_GotMouseCapture(object sender, MouseEventArgs e)
+        {
+            try
+            {
+                client.Write("1");
+            }
+            catch { }
+            
+        }
+
+        private void Button_GotMouseCapture_1(object sender, MouseEventArgs e)
+        {
+            try
+            {
+                client.Write("disconnect");
+            }
+            catch { }
+            
+        }
+
+        private void Connect_GotMouseCapture(object sender, MouseEventArgs e)
+        {
+            try
+            {
+                client.Connect("127.0.0.1", 5000);
+            }
+            catch { }
+        }
+    }
+}
---
+++ Server/Program.cs
@@ -0,0 +1,64 @@
+﻿
+
+
+
+
+using SimpleTCP;
+using System.Text;
+
+List<string> citation = new List<string>() { "Life is what happens when you’re busy making other plans",
+"When the going gets tough, the tough get going", "You must be the change you wish to see in the world",
+"You only live once, but if you do it right, once is enough", "Tough times never last but tough people do",
+"Get busy living or get busy dying", "Whether you think you can or you think you can’t, you’re right", "Tis better to have loved and lost than to have never loved at all"};
+
+
+if(!File.Exists("log.txt"))
+{
+    File.Create("log.txt");
+}
+
+
+var server = new SimpleTcpServer();
+
+server.ClientConnected += (sender, e) =>
+{
+    using (StreamWriter writer = File.AppendText("log.txt"))
+    {
+         writer.WriteLine($"\n\nClient ({e.Client.RemoteEndPoint}) connected!\nTime: {DateTime.Now.ToShortTimeString()}");
+    }
+};
+
+server.ClientDisconnected += (sender, e) =>
+{
+    using (StreamWriter writer = File.AppendText("log.txt"))
+    {
+        writer.WriteLine($"\n\nClient ({e.Client.RemoteEndPoint}) disconnected!\nTime: {DateTime.Now.ToShortTimeString()}");
+    }
+};
+
+server.DataReceived += (sender, e) =>
+{
+    var ep = e.TcpClient.Client.RemoteEndPoint;
+    var msg = Encoding.UTF8.GetString(e.Data);
+    if (!msg.Contains("disconnect"))
+    {
+        Random rn = new Random();
+        
+        ;
+        string s = citation[rn.Next(0, citation.Count - 1)];
+        e.Reply(s);
+        using (StreamWriter writer = File.AppendText("log.txt"))
+        {
+            writer.WriteLine($"\n\nClient {ep} recieved:   {s}");
+        }
+    }
+    else
+    {
+        e.Reply("-1");
+    }
+};
+
+
+server.Start(5000);
+
+Console.ReadLine();
